abfc38d46d513923ec7456514ec463fd5f844ce7
John Xander <johnblox5@gmail.com>
2022-05-28 17:06:47 +0100

    Initial logic and layout

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Initial Commit
+
+const textarea = document.getElementById("textarea")
+const generateName = document.getElementById("generateName")
+const randomName = document.getElementById("randomName")
+
+generateName.addEventListener("click", () => {
+    const arrOfNames = textarea.value.split("\n")
+    const randomNum = Math.floor(Math.random() * arrOfNames.length)
+    const currentName = arrOfNames[randomNum]
+    const filtered = arrOfNames.filter(name => name !== currentName)
+
+    randomName.innerHTML = currentName    
+    textarea.value = filtered.join("\n")
+})
+
